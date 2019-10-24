6363be798f5c346e2d3bc22c4afe7e077e0b00bd
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-10-24 16:15:15 -0300

    CM - EF Core configured

---
+++ Infrastructure/Contexts/SanFranciscoSchoolContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SanFranciscoSchool.Models;
+
+namespace SanFranciscoSchool.Infrastructure.Contexts
+{
+    public class SanFranciscoSchoolContext : DbContext
+    {
+        public DbSet<SchoolClass> SchoolClasses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public SanFranciscoSchoolContext(DbContextOptions<SanFranciscoSchoolContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20191024191311_Initial.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SanFranciscoSchool.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SchoolClasses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Identification = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchoolClasses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Score = table.Column<int>(nullable: false),
+                    SchoolClassId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_SchoolClasses_SchoolClassId",
+                        column: x => x.SchoolClassId,
+                        principalTable: "SchoolClasses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SchoolClassId",
+                table: "Students",
+                column: "SchoolClassId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "SchoolClasses");
+        }
+    }
+}
