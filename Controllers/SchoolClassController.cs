659e2cb9da9cf6aee87001ee3e7a437e3a3ed599
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-10-24 17:08:23 -0300

    CM - Controllers added

---
+++ Controllers/SchoolClassController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using SanFranciscoSchool.Infrastructure.Contexts;
+using SanFranciscoSchool.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SanFranciscoSchool.Controllers
+{
+    [Route("[controller]")]
+    public class SchoolClassController : Controller
+    {
+        private readonly SanFranciscoSchoolContext _dbContext;
+
+        public SchoolClassController(SanFranciscoSchoolContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        [EnableQuery()]
+        public IEnumerable<SchoolClass> Get()
+        {
+            return _dbContext.SchoolClasses.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] SchoolClass schoolClass)
+        {
+            _dbContext.SchoolClasses.Add(schoolClass);
+            _dbContext.SaveChanges();
+
+            return new NoContentResult();
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using SanFranciscoSchool.Infrastructure.Contexts;
+using SanFranciscoSchool.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SanFranciscoSchool.Controllers
+{
+    [Route("[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly SanFranciscoSchoolContext _dbContext;
+
+        public StudentController(SanFranciscoSchoolContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        [EnableQuery()]
+        public IEnumerable<Student> Get()
+        {
+            return _dbContext.Students.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            _dbContext.Students.Add(student);
+            _dbContext.SaveChanges();
+
+            return new NoContentResult();
+        }
+    }
+}
