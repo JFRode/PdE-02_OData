9a7732a8c13265fa6459bd053264a3b0f52816f6
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-10-24 16:14:49 -0300

    CM - Models added

---
+++ Models/SchoolClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SanFranciscoSchool.Models
+{
+    public class SchoolClass
+    {
+        public Guid Id { get; set; }
+        public string Identification { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SanFranciscoSchool.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public Guid SchoolClassId { get; set; }
+        public SchoolClass SchoolClass { get; set; }
+    }
+}
